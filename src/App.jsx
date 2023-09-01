429b071bc0f41b9006827b3805ebe12b2bf29a1b
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-09-01 06:54:38 +0000

    Navbar completed and added images/logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./utils";
 const App = () => {
   return (
     <BrowserRouter>
       <Box sx={{backgroundColor:"#000"}}>
         <Navbar/>
         <Routes>
-          <Route path="/" exact element={<Feed />} />
+          {/* <Route path="/" exact element={<Feed />} /> */}
           <Route path="/video/:id" exact element={<VideoDetail />} />
           <Route path="/channel/:id" exact element={<ChannelDetail />} />
           <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+// Export Pages
+
+export {default as Navbar} from "../components/Navbar"
+export {default as Feed} from "../pages/Feed"
+export {default as VideoDetail} from "../pages/VideoDetail"
+export {default as ChannelDetail} from "../pages/ChannelDetail"
+export {default as SearchFeed} from "../components/SearchFeed"
+
+
+// Export Components
+export {default as SearchBar} from "../components/SearchBar"
